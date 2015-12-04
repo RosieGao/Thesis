1f28ad92f51b18fc9176e5f5993e0edf0e2fd9fd
RosieGao <gym9255@gmail.com>
2015-12-04 03:39:28 -0700

    resize canvas done

--- js/VisualBootStrap.js
+++ js/VisualBootStrap.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
 	$(".content").css("min-height", $(window).height() - 110);
-	console.log("aaa");
+	$(".fullScreen").on("click", function(){$(".content").removeClass("lgSize mdSize smSize xsSize")});
+	$(".lgButton").on("click", function(){$(".content").removeClass("lgSize mdSize smSize xsSize"); $(".content").addClass("lgSize")});
+	$(".mdButton").on("click", function(){$(".content").removeClass("lgSize mdSize smSize xsSize"); $(".content").addClass("mdSize")});
+	$(".smButton").on("click", function(){$(".content").removeClass("lgSize mdSize smSize xsSize"); $(".content").addClass("smSize")});
+	$(".xsButton").on("click", function(){$(".content").removeClass("lgSize mdSize smSize xsSize"); $(".content").addClass("xsSize")});
 })
